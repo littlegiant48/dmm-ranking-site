61c1e317727cfc3a4f1fd2a9ca95a9cdd41db57c
littlegiant48 <tips.afuri@gmail.com>
2025-12-02 20:22:48 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-API_ID = "tCTaa51SgTZv1nPkFmx1"
-AFF_ID = "FANZAer-990"
+# GitHub Actionsで設定したSecretsから読み込む
+API_ID = os.environ["DMM_API_ID"]
+AFF_ID = os.environ["DMM_AFF_ID"]
 
 # 取得するカテゴリ例（アダルト動画）
 SERVICE = "digital"
